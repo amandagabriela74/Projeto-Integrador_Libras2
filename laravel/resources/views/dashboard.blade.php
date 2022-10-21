c6d68abbdaa30d9cccb70db9be18d5f461d1492b
Amanda gabriela <amandagabriela056@gmail.com>
2022-10-20 23:08:02 -0300

    começando estilar pelo tailwind css

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -4,41 +4,43 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    
-
-                    <table class=" gap-2 columns-2 md:columns-3 lg:columns-4">
-                        <thead>
-                          <tr >
-                                <th scope="col">#</th>
-                                <th scope="col">Título</th>
-                                <th scope="col">Tópico</th>
-                                <th scope="col">Embed</th>
-                                <th scope="col">Descrição</th>
-                                <th scope="col">...</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @foreach(\App\Models\Curso::all() as $curso)
-                          <tr class="border border-black">
-                                <th>{{$curso->id}}</th>
-                                <th>{{$curso->titulo}}</th>
-                                <th>{{$curso->topico}}</th>
-                                <th>{{$curso->embed}}</th>
-                                <th>{{$curso->descricao}}</th>
-
-                          </tr>
-                          @endforeach
-                        </tbody>
-
-
-
+                    <div class="flex flex-row  gap-2 columns-2 md:columns-3 lg:columns-4">
+                        <div class=" box-content h-90 w-40  p-4 border-4 ">
+                            <h1>Módulo 1</h1>
+                            <p>• Alfabeto</p>
+                            <p>• Numerais</p>
+                            <p>• Saudações</p>
+                            <p>• Calendádio</p>
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">Inscrever-se</button>
+                        </div>
+                        <div class=" box-content h-90 w-40  p-4 border-4 ">
+                            <h3>Módulo 2</h3>
+                            <p>• Cores</p>
+                            <p>• Verbos</p>
+                            <p>• Pronomes</p>
+                            <p>• Família</p>
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">Inscrever-se</button>
+                        </div>
+                        <div class=" box-content h-90 w-40  p-4 border-4 ">
+                            <h3>Módulo 3</h3>
+                            <p>• Frutas</p>
+                            <p>• Advérbio de Tempo</p>
+                            <p>• Profissões</p>
+                            <p>• Reino Animal</p>
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">Inscrever-se</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class=" box-content h-32 w-32 p-4 border-4 bg-red-500"></div>
+    <span class="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 ">
+        Hello <br>
+        World
+    </span>
 </x-app-layout>
